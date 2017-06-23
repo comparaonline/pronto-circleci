76fe3bcfbeb14a3960223ed358d0a9638adfe194
Joaquín Moreira <jmoreiras@gmail.com>
2017-06-23 09:12:33 -0400

    Initial bundler gem generated structure

---
+++ lib/pronto/circleci.rb
@@ -0,0 +1,7 @@
+require "pronto/circleci/version"
+
+module Pronto
+  module Circleci
+    # Your code goes here...
+  end
+end
---
+++ lib/pronto/circleci/version.rb
@@ -0,0 +1,5 @@
+module Pronto
+  module Circleci
+    VERSION = "0.1.0"
+  end
+end
