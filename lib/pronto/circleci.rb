ddfc76fe1fef4e5d0897d07d67dcbb695ba9f4aa
Joaquín Moreira <jmoreiras@gmail.com>
2017-07-05 10:02:51 -0400

    Implement executable

--- lib/pronto/circleci.rb
+++ lib/pronto/circleci.rb
@@ -1,7 +1,21 @@
 require "pronto/circleci/version"
+require 'pronto/circleci/runner'
 
 module Pronto
-  module Circleci
-    # Your code goes here...
+  module CircleCI
+    module_function
+
+    def run
+      Runner.new(pull_requests_urls)
+    end
+
+    def pull_requests_urls
+      ENV['CI_PULL_REQUESTS'].split(',')
+    end
+
+    def gem_root
+      return "#{Rails.root}" if defined?(Rails)
+      File.expand_path('../../../', __FILE__)
+    end
   end
 end
