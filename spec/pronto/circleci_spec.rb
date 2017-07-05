d99ca92a1fb975fc343ee57d628610f4bf86a393
Joaquín Moreira <jmoreiras@gmail.com>
2017-07-05 10:02:16 -0400

    Implement specs

--- spec/pronto/circleci_spec.rb
+++ spec/pronto/circleci_spec.rb
@@ -1,11 +1,37 @@
-require "spec_helper"
+require 'spec_helper'
 
-RSpec.describe Pronto::Circleci do
-  it "has a version number" do
-    expect(Pronto::Circleci::VERSION).not_to be nil
+RSpec.describe Pronto::CircleCI do
+  it 'has a version number' do
+    expect(Pronto::CircleCI::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#pull_requests_urls' do
+    it 'parse env variable to an array of url strings' do
+      expect(described_class.pull_requests_urls).to eq(
+        GithubMock.pull_requests_urls
+      )
+    end
+  end
+
+  describe '#gem_root' do
+    context 'on a rails app' do
+      let(:path) { 'rails' }
+      before(:example) do
+        stub_const('Rails', Class.new)
+        allow(Rails).to receive('root') { path }
+      end
+
+      it 'returns rails folder' do
+        expect(described_class.gem_root).to eq(path)
+      end
+    end
+
+    context 'outside a rails app' do
+      let(:path) { File.expand_path('../../../', __FILE__) }
+
+      it 'returns the root of the app' do
+        expect(described_class.gem_root).to eq(path)
+      end
+    end
   end
 end
---
+++ spec/pronto/lib/runner_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+
+RSpec.describe Pronto::CircleCI::Runner do
+  def mock_flags(flags, value)
+    flags.each do |flag|
+      allow(Pronto::CircleCI::Config.instance)
+      .to receive(:get)
+      .with('pronto', flag)
+      .and_return value
+    end
+  end
+
+  def mock_default_config
+    allow(Pronto::CircleCI::Config.instance)
+      .to receive(:get)
+      .and_wrap_original do |method, *args|
+        case args
+        when ['config', 'org'] then GithubMock.ORG
+        when ['config', 'repo'] then GithubMock.REPO
+        else method.call(*args)
+        end
+      end
+  end
+
+  def command_with_flags(pull_request, flags_line)
+    cmd = Pronto::CircleCI::Runner::COMMAND % {
+      pull_request_id: pull_request.id,
+      flags: flags_line,
+      base_branch: pull_request.base_branch
+    }
+  end
+
+  before(:context) do
+    GithubMock.pull_request_apis
+  end
+
+  before(:example) do
+    mock_default_config
+  end
+
+  describe '#run' do
+    it "calls 'command_line' once per pull request" do
+      pull_requests_urls = GithubMock.pull_requests_urls
+      expect(described_class).to receive(:command_line).exactly(
+        pull_requests_urls.length
+      ).times
+      described_class.run(GithubMock.pull_requests_urls)
+    end
+  end
+
+  describe '#command_line' do
+    before(:example) do
+      mock_flags(%w(comments_on_diff reviews_on_diff report_status), false)
+    end
+
+    context 'with all pronto config in false' do
+      let(:pull_request) {
+        pr_url = GithubMock.pull_request_url
+        Pronto::CircleCI::PullRequest.load_from_url(pr_url)
+      }
+
+      it 'only sets default flag' do
+        cmd = command_with_flags(pull_request, 'github')
+        expect(described_class.command_line(pull_request)).to eq(cmd)
+      end
+
+      it 'set a specific flag' do
+        mock_flags(%w(reviews_on_diff), true)
+        cmd = command_with_flags(pull_request, 'github_pr_review')
+        expect(described_class.command_line(pull_request)).to eq(cmd)
+      end
+
+      it 'sets two flags' do
+        mock_flags(%w(comments_on_diff reviews_on_diff), true)
+        cmd = command_with_flags(pull_request, 'github_pr github_pr_review')
+        expect(described_class.command_line(pull_request)).to eq(cmd)
+      end
+    end
+  end
+end
