cc8f7da7e94df92094759202a4b8ff363a0853f4
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2017-10-10 09:50:17 +0300

    Rename access token env variable to PRONTO_GITHUB_ACCESS_TOKEN
    By adding PRONTO_ to GITHUB_ACCESS_TOKEN
    Previously only renamed in README, assuming there's no usage in code.
    https://github.com/comparaonline/pronto-circleci/pull/2

--- spec/support/environment.rb
+++ spec/support/environment.rb
@@ -1,4 +1,4 @@
 require_relative 'github_mock'
 
-ENV['GITHUB_ACCESS_TOKEN'] = GithubMock.access_token
+ENV['PRONTO_GITHUB_ACCESS_TOKEN'] = GithubMock.access_token
 ENV['CI_PULL_REQUESTS'] = GithubMock.pull_requests_urls.join(',')
